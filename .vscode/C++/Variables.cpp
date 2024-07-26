98c9736cabe47875fd57d5ed84ecb6546d0a1f21
Evan Johanns <evanjohanns00@gmail.com>
2024-07-25 22:13:12 -0400

    adding new file Excersize-1_Cout.cpp

---
+++ .vscode/C++/Variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int myNum = 15;
+    float myFloat = 2.4;
+    double myDouble = 0.0017;
+    std:: cout << myNum << std::endl;
+    std:: cout << myFloat << std::endl;
+    std:: cout << myDouble << std::endl;
+    // std:: cout << myText;
+    return 0;
+}
